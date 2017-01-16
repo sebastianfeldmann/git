bd3bfb4e0f5a49e902fde9498049bae6f7353bfc
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2017-01-16 01:47:17 +0100

    Add root path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,13 @@
 /**
  * This file is part of SebastianFeldmann\Git.
  *
- * (c) Sebastian Feldmann <sf@sebastian.feldmann.info>
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
+define('SF_GIT_PATH_ROOT', realpath(__DIR__ . '/..'));
 define('SF_GIT_PATH_FILES', realpath(__DIR__ . '/files'));
 
 require __DIR__ . '/../vendor/autoload.php';
