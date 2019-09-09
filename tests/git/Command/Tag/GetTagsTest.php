9d43df184f80daaa4303f0a96f2b44884185b2fe
Sebastian Feldmann <sebastianfeldmann@users.noreply.github.com>
2019-09-09 11:46:39 +0200

    Add getTags command
    With this you can get the tags for a specific commit. Other than
    'getCurrentTag' that tries to find and return a tag even if the
    current commit is not tagged, 'getTags' will return an empty list if
    no tag is found on the given commit.

---
+++ tests/git/Command/Tag/GetTagsTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of SebastianFeldmann\Git.
+ *
+ * (c) Sebastian Feldmann <sf@sebastian-feldmann.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace SebastianFeldmann\Git\Command\Describe;
+
+use PHPUnit\Framework\TestCase;
+use SebastianFeldmann\Git\Command\Tag\GetTags;
+
+/**
+ * Class GetTagsTest
+ *
+ * @package SebastianFeldmann\Git
+ * @author  Sebastian Feldmann <sf@sebastian-feldmann.info>
+ * @link    https://github.com/sebastianfeldmann/git
+ * @since   Class available since Release 2.3.0
+ */
+class GetTagsTest extends TestCase
+{
+    /**
+     * Tests GetTags::getGitCommand
+     */
+    public function testGetTagsDefault()
+    {
+        $cmd = new GetTags();
+        $exe = $cmd->getCommand();
+
+        $this->assertEquals('git tag --points-at \'HEAD\'', $exe);
+    }
+
+    /**
+     * Tests GetTags::getGitCommand
+     */
+    public function testGetTagsFromHash()
+    {
+        $cmd = new GetTags();
+        $cmd->pointingTo('1234567890');
+        $exe = $cmd->getCommand();
+
+        $this->assertEquals('git tag --points-at \'1234567890\'', $exe);
+    }
+}
